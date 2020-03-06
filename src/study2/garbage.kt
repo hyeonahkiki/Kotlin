63342eb8fdd3fb95a32094f20f4525ca7f48abaa
hyeonahkiki <kkongc7@gmail.com>
2020-03-06 19:13:53 +0900

    memory-heap(save str) & garbage collection

--- src/study2/garbage.kt
+++ src/study2/garbage.kt
@@ -1,2 +1,12 @@
 package study2
 
+fun main(args:Array<String>):Unit{
+    var res = ""
+    var a = 1
+
+    while(a <= 100 ){
+        res += "$a"
+        a += 1
+    }
+    println(res)
+}
--- src/study2/strMemory.kt
+++ src/study2/strMemory.kt
@@ -1,2 +1,8 @@
 package study2
 
+fun main(args:Array<String>):Unit{
+    var firstVar = "hello"
+    var secondVar = "Kotlin"
+
+    firstVar += secondVar
+}
