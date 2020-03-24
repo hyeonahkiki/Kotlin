a2310b8e69b4dd37dbca6ec481c3a1c98cef0960
hyeonahkiki <kkongc7@gmail.com>
2020-03-24 17:30:28 +0900

    constructor-init

--- src/study2/initBlock.kt
+++ src/study2/initBlock.kt
@@ -1,2 +1,24 @@
 package study2
 
+// constructor는 생략이 가능하다.
+class Rectangle(width:Int, height:Int)
+{
+    // width는 초기화
+    val width = width
+    // init블록을 이용해서 초기화
+    val height : Int
+
+    // init블록은 여러개로 나눠서 실행가능
+    init{
+        this.height = height
+    }
+    val size: Int
+
+    init{
+        size = width * height
+    }
+}
+fun main(args:Array<String>):Unit{
+    val rectangle = Rectangle(5,6)
+    println(rectangle.size)
+}
