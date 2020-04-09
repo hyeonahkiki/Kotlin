59345d5a5b8b04e580bc67fd2ca5f1a4b48fabb3
hyeonahkiki <kkongc7@gmail.com>
2020-04-09 22:12:42 +0900

    object를 이용한 상속

--- src/study3/inheritance2.kt
+++ src/study3/inheritance2.kt
@@ -1,2 +1,29 @@
 package study3
 
+// 클래스 없이 특정한 클래스를 상속받는 객체 만들기
+
+open class ParentClass(val name:String, val age:Int)
+{
+    open fun output()
+    {
+        println("이름: ${name}")
+        println("나이: ${age}")
+    }
+}
+
+// 상속받는 방법1
+class ChildClass(name:String, age:Int):ParentClass(name, age)
+
+// 상속받는 방법2
+fun main(args:Array<String>):Unit
+{
+    // 상속을 받는 object
+    val childClass:ParentClass = object: ParentClass("이태일", 31)
+    {
+        override fun output()
+        {
+            println("ParentClass를 오버라이딩한 함수")
+        }
+    }
+    childClass.output()
+}
