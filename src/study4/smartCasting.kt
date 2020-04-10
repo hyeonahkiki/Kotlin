5cf14fe0dfc201c1c9abde708fa7f08bb05f4686
hyeonahkiki <kkongc7@gmail.com>
2020-04-10 23:36:49 +0900

    smart casting 이해

--- src/study4/smartCasting.kt
+++ src/study4/smartCasting.kt
@@ -2,7 +2,7 @@ package study4
 
 /*
     is연산자 : 타입을 확인하는 연산자
-    java의 경우 instanceof 비슷함
+    java의 경우 instanceof 비슷하다고 생각하면됨
  */
 fun smartCast(data:Any):Int{
     // is연산자를 거치면서 any 타입은 수식계산 안됨
